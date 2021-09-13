2d040ca628b631029ab0a33556a73cecde3724b7
Alessio <alessio.livolsi.27@gmail.com>
2021-09-13 17:03:25 +0100

    Logic works for hitting rest api and taking screenshot

--- script.py
+++ script.py
@@ -2,7 +2,14 @@ import requests
 from requests.models import HTTPBasicAuth
 import json
 from pprint import pprint
+from datetime import datetime
+import html
 
+
+from selenium import webdriver
+import datetime, time, pyautogui
+import os
+from webdriver_manager.chrome import ChromeDriverManager
 
 r = requests.get(
     "http://127.0.0.1:8000/api/url/",
@@ -11,20 +18,25 @@ r = requests.get(
 
 response = r.json()
 
-
 for url in response["results"]:
-    print("url found")
-    pprint(url["url"])
+    name = url["website"]
+    driver = webdriver.Chrome(ChromeDriverManager().install())
+    now = datetime.datetime.now()
+    driver.maximize_window()
+    driver.get(url["url"])
+    time.sleep(1.5)
+    pic = pyautogui.screenshot()
+    filename = "image.jpeg"
+    pic.save(filename)
+    driver.close()
+    time.sleep(1)
+    upload = {"image": open("image.jpeg", "rb")}
 
+    data = {"url": url["id"], "created": datetime.datetime.now()}
 
-# if response.status_code == 200:
-#     print("The request was a success!")
-#     # Code here will only run if the request is successful
-# elif response.status_code == 404:
-#     print("Result not found")
-#     # Code here will react to failed requests
-
-
-# TODO: once response is done via the GET method, loop through the response and for each iteration,
-# take the screenshot and then send it to the screenshot rest end point, when sent screenshot to end point include URL,
-# so we understand what website it is.
+    resp = requests.post(
+        "http://127.0.0.1:8000/api/screenshot/",
+        files=upload,
+        data=data,
+        auth=HTTPBasicAuth("user_screenshot", "myvoiceismypassword"),
+    )
